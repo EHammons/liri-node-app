21bcf905a5e2098e7125c43d0fd3dfca2b2e9fcc
Stardust <52759497+EHammons@users.noreply.github.com>
2019-10-02 22:04:06 -0500

    movie-this now works

--- liri.js
+++ liri.js
@@ -10,6 +10,9 @@ var command = process.argv[2];
 var subject = process.argv.slice(3).join(" ");
 
 function concert() {
+    if (!subject) {
+        subject = "Celine Dion"
+    };
     axios.get("https://rest.bandsintown.com/artists/" + subject + "/events?app_id=codingbootcamp")
     .then(function(response) {
         for (i = 0; i < 5; i++) {
@@ -28,6 +31,38 @@ function song() {
        
       console.log(response.tracks.items[0]);
       });
+};
+
+function movie() {
+    if (!subject) {
+        subject = "Mr. Nobody"
+    };
+    var queryUrl = "http://www.omdbapi.com/?t=" + subject + "&y=&plot=short&apikey=trilogy";
+    axios.get(queryUrl).then(
+        function(response) {
+          console.log("\nMovie Title: " +response.data.Title + "\nRelease Year: " +response.data.Year + "\nIMDB Rating: " +response.data.imdbRating + "\nRotten Tomatoes Rating: " +response.data.Ratings[1].Value + "\nCountry: " +response.data.Country + "\nLanguage: " +response.data.Language + "\nPlot: " +response.data.Plot + "\nActors: " +response.data.Actors);
+        })
+        .catch(function(error) {
+          if (error.response) {
+            // The request was made and the server responded with a status code
+            // that falls out of the range of 2xx
+            console.log("---------------Data---------------");
+            console.log(error.response.data);
+            console.log("---------------Status---------------");
+            console.log(error.response.status);
+            console.log("---------------Status---------------");
+            console.log(error.response.headers);
+          } else if (error.request) {
+            // The request was made but no response was received
+            // `error.request` is an object that comes back with details pertaining to the error that occurred.
+            console.log(error.request);
+          } else {
+            // Something happened in setting up the request that triggered an Error
+            console.log("Error", error.message);
+          }
+          console.log(error.config);
+        });
+      
 }
 
 switch(command) {
